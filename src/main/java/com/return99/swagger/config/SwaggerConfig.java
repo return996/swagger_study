41636caff2602d63bd1b95408c0aa828ed3d689e
return996 <2208604865@qq.com>
2020-05-26 15:33:22 +0800

    commit

--- src/main/java/com/return99/swagger/config/SwaggerConfig.java
+++ src/main/java/com/return99/swagger/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.return99.swagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -18,9 +21,29 @@ public class SwaggerConfig {
 
     // 配置了Swagger的Docket的bean实例
     @Bean
-    public Docket docket() {
+    public Docket docket(Environment environment) {
+
+        // 这是要显示的Swagger环境
+        Profiles profiles = Profiles.of("dev","test");
+
+        // 获取项目的环境：
+        boolean flag = environment.acceptsProfiles(profiles);
+        System.out.println(flag);
+
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .enable(true)
+                .select()
+                // RequestHandlerSelectors 配置要扫描的接口方式
+                // basePackage:指定要扫描的包
+                // any():扫描全部
+                // none():不扫描
+                // withClassAnnotation: 扫描类上的注解,参数是一个注解的反射对象
+                // withMethodAnnotation: 扫描方法上的注解
+                .apis(RequestHandlerSelectors.basePackage("com.return99.swagger.controller"))
+                // paths() 过滤什么路径
+                //.paths(PathSelectors.ant("/return99/**"))
+                .build();
     }
 
     // 配置Swagger信息=apiInfo
