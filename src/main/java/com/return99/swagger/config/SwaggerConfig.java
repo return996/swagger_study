bf72d070e2f846a77dfa99946c30aedf8735ee0f
return996 <2208604865@qq.com>
2020-05-26 14:47:04 +0800

    change SwaggerConfig

--- src/main/java/com/return99/swagger/config/SwaggerConfig.java
+++ src/main/java/com/return99/swagger/config/SwaggerConfig.java
@@ -1,9 +1,42 @@
 package com.return99.swagger.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.service.ApiInfo.DEFAULT_CONTACT;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    // 配置了Swagger的Docket的bean实例
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    // 配置Swagger信息=apiInfo
+    private ApiInfo apiInfo() {
+
+        Contact contact = new Contact("return996", "https://www.bytedance.com/zh", "2208604865@qq.com");
+
+        return new ApiInfo(
+                "return996的SwaggerAPI文档",
+                "即使再小的帆也能远航",
+                "1.0",
+                "https://www.baidu.com",
+                DEFAULT_CONTACT,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+    }
+
 }
